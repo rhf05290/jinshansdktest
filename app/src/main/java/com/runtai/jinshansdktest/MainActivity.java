3ca002fdeab21a7e9c2ac61a4cacde4177cd5592
rhf05290 <rhf05290@163.com>
2017-05-10 12:15:33 +0800

    Initial commit

---
+++ app/src/main/java/com/runtai/jinshansdktest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.runtai.jinshansdktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+    }
+}
